acf4a6881ee0cbbc2138614fa53efae26f36ebe3
git <richard.james.grove@gmail.com>
2023-09-20 16:07:38 +0100

    update

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 function quiz() {
   let name = prompt("what is your name");
+  let Game = prompt("let the games begin")
+  while (!user) {
+    user = prompt("You need to play the game.");
 
   // let color = prompt("What is your favorite colour?");
 
@@ -47,39 +50,78 @@ function quiz() {
   //     default:
   //         console.log(`I don't know what kind of color is that!!!!)
 
-  let film = prompt(
-    "Return of the jedi the greatest Star wars film of all time??"
-  );
+  let film = prompt("Is Star wars one of my favourite movies??");
   if (film.toLowerCase() === "yes") {
     alert("YOU ARE ONE WITH THE FORCE!!");
   } else if (film.toLowerCase() === "no") {
     alert("I have failed you Anakin, I have failed you!");
   }
 
-  let food = prompt("Italian is the best food??");
+  let food = prompt("Mexican is one of my favourite foods??");
   if (food.toLowerCase() === "yes") {
-    alert("Cant beat a good carbonara!!");
+    alert(" Its not going in this stomach!!");
   } else if (food.toLowerCase() === "no") {
     alert("What kind of meatball do you think this is!!!");
   }
   let outdoors = prompt(
-    "You cant beat getting outside and walking about in the coutryside??"
+    "I prefer to sit on my sofa than to get out and go for a walk??"
   );
   if (outdoors.toLowerCase() === "yes") {
-    alert("No complaints here!!");
+    alert("WHAT DO YOU THINK I AM SOME KIND OF COUCH POTATO!!");
   } else if (outdoors.toLowerCase() === "no") {
-    alert("WHAT ARE YOU A COUCH POTATO!");
+    alert("No complaints here lets get out! !");
   }
-  let xbox = prompt("Xbox is the best console??");
+  let xbox = prompt("Do I prefer xbox over playstation??");
   if (xbox.toLowerCase() === "yes") {
     alert("Master Chief is awaiting our arrival!!");
   } else if (xbox.toLowerCase() === "no") {
     alert("What do you know peseant!");
   }
-  let country = prompt("Can you think of a question??");
-  if (country.toLowerCase() === "yes") {
-    alert("Someone got some sleep last night!!");
-  } else if (country.toLowerCase() === "no") {
-    alert("Glad we are in the same boat!!");
+  let question = prompt("Is my favourite colour aqua blue??");
+  if (question.toLowerCase() === "yes") {
+    alert("I mean who doesnt like aqua blue!!");
+  } else if (question.toLowerCase() === "no") {
+    alert("Whats wrong with you try again!!");
   }
+
+  }
+  let question = prompt("Which number in my top ten is a water falls at Platres pick between 1 and 10 ")
+if (question.toLowerCase() = "9") {
+alert("Your right its a beautiful waterfall in cyrpus ")
+}  else if (question.toLowerCase() === "1") {
+  alert(" too low try again!!"); 
+} else if (question.toLowerCase() === "2") {
+  alert("too low try again!!");
+} else if (question.toLowerCase() === "3") {
+  alert("too low try again!!");
+} else if (question.toLowerCase() === "") {
+  alert("too low try again!!");
+} else if (question.toLowerCase() === "5") {
+  alert("too low try again!!");
+} else if (question.toLowerCase() === "6") {
+  alert("too low try again!!");
+} else if (question.toLowerCase() === "7") {
+  alert("too low try again!!");
+} else if (question.toLowerCase() === "8") {
+  alert("too low try again!!");
+} else if (question.toLowerCase() === "10") {
+  alert("too high try again!!");
+}
+
+let movie = prompt("what are some of my favourite films?")
+if (question.toLowerCase() = "Jurassic park") {
+  alert("Correct t ")
+  }  else if (question.toLowerCase() === "1") {
+    if (question.toLowerCase() = "9") {
+      alert("Your right its a beautiful waterfall in cyrpus ")
+      }  else if (question.toLowerCase() === "1") {
+        if (question.toLowerCase() = "9") {
+          alert("Your right its a beautiful waterfall in cyrpus ")
+          }  else if (question.toLowerCase() === "1") {
+            if (question.toLowerCase() = "9") {
+              alert("Your right its a beautiful waterfall in cyrpus ")
+              }  else if (question.toLowerCase() === "1") {
+
+
+
 }
